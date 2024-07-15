b01ef646a92794b7683ec222d5bf739b26494333
AyRawat <aayushrawat20@gmail.com>
2024-07-16 04:59:21 +0530

    added-experience

--- src/data/index.js
+++ src/data/index.js
@@ -1,8 +1,4 @@
-import {
-  algorithms,
-  devnotes,
-  oscs,
-} from "../assets";
+import { algorithms, devnotes, oscs } from "../assets";
 
 export const navLinks = [
   {
@@ -25,43 +21,52 @@ export const navLinks = [
 
 const experiences = [
   {
-    title: "YouTube Content Creator",
+    title: "Software Engineer",
     company_name: "Self Employed",
-    date: "2016 - Present",
+    date: "2024 - Present",
     details: [
-      "Built a subscriber base of over <span style='color: white;'>500,000 subscribers</span> by creating video content to help programmers.",
-      "Crafted visually appealling programming videos that have garnered over <span style='color: white;'>30,000,000 views</span>.",
-      "Produced high-quality educational and entertaining videos for clients including <span style='color: white;'>Intel, JetBrains, and MicroCenter</span>.",
+      "Built an <span style='color: white;'>Automated Trading Journal</span> for keeping a track of all the candidates, and generated a report showing their emotional side while talking a trade.",
+      "Crafted a trading strategy for the Researcher team that generated over <span style='color: white;'>400,000 $</span>.",
     ],
   },
   {
     title: "Software Developer",
-    company_name: "Indie",
-    date: "2019 - 2023",
+    company_name: "Adani",
+    date: "2023 - 2024",
     details: [
-      "Developed and delivered custom interdisciplinary coding portfolio for clients including <span style='color: white;'>Nvidia, Hostinger, and Amazon</span>.",
-      "<span style='color: white;'>Designed and developed innovative</span> AI applications and interactive websites.",
+      "Developed and delivered BBPS for one of the biggest conglomerates for easy bill payments, using <span style='color: white;'>Node.js Typescript and React Redux</span>.",
+      "Successfully launched the app for <span style='color: white;'>16M</span> users across India, with <span style='color: white;'>5 out of 26</span> major categories",
+      "Integrated Loyalty systems for customer retention",
       "<span style='color: white;'>Managed full project lifecycle</span> from concept to deployment in successful and timely project completions.",
     ],
   },
   {
     title: "Software Engineer",
-    company_name: "Prime 3",
-    date: "2018 - 2019",
+    company_name: "Nova Benefits",
+    date: "2022 - 2023",
     details: [
-      "Built custom enterprise applications for a <span style='color: white;'>Fortune 500 company</span> as a full-stack software engineer.",
-      "Developed and maintained <span style='color: white;'>scalable backend services</span>, ensuring high availability for critical business applications.",
+      "Built a custom e-card generator for one of the <span style='color: white;'>Unicorns company</span> as a full-stack software engineer.",
+      "Helped in the biggest migration from CRM to internal systems <span style='color: white;'>scalable backend services</span>, ensuring high availability for critical business applications.",
+      "<span style='color: white;'>Collaborated with a team</span> to design and implement front-end interfaces.",
+    ],
+  },
+  {
+    title: "Software Engineer",
+    company_name: "Tech Ferry",
+    date: "2021 - 2022",
+    details: [
+      "Built a custom e-card generator for one of the top<span style='color: white;'>Unicorns</span> as a full-stack software engineer.",
+      "Helped in the biggest <span style='color: white;'>migration</span> from CRM to internal systems for <span style='color: white;'>scalable backend services</span>, ensuring high availability for critical business applications.",
       "<span style='color: white;'>Collaborated with a team</span> to design and implement front-end interfaces.",
     ],
   },
   {
     title: "Computer Science",
-    company_name: "ODU",
-    date: "2015 - 2018",
+    college: "DIT University",
+    date: "2016 - 2020",
     details: [
-      "Built a <span style='color: white;'>computer science foundation</span> learning theory, computer architecture, and software engineering.",
-      "Worked and interned at <span style='color: white;'>NASA and Norfolk Southern Railway</span> to gain practical experience in the field of data analysis.",
-      "Acted as a member of the <span style='color: white;'>Association for Computing Machinery</span> (ACM).",
+      "Built a <span style='color: white;'>chat application for mute people<span style='color: white;'>, where the text gets translated from hand signs to textual data",
+      "Worked and interned at <span style='color: white;'>THDC</span> a goverment body that looks after hydel power generation to gain practical experience in the field of data analysis.",
     ],
   },
 ];
@@ -88,4 +93,3 @@ const portfolio = [
 ];
 
 export { experiences, portfolio };
-
